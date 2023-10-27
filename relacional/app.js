f133f0c2eaa9754164bcf101ce49df9afbd05eb2
Poltergeis <julio.cruzazul@hotmail.com>
2023-10-26 23:01:16 -0600

    construida la conexion en app.js

---
+++ relacional/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'redSocial'
+});
+
+app.listen(3991,() => {
+    console.log("corriendo en el puerto 3991");
+});
