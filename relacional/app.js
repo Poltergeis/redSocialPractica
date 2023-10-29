0df8de273dc6152482dabd95edc7e91e8c098fc6
Poltergeis <julio.cruzazul@hotmail.com>
2023-10-28 18:12:17 -0600

    he terminado de configurar app.js de la api de mysql

--- relacional/app.js
+++ relacional/app.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mysql = require("mysql");
+const rutasComentarios = require("./routes/rutasComentarios");
+const rutasUsuarios = require("./routes/rutasUsuario");
+const rutasPublicaciones = require("./routes/rutasPublicaciones");
+const Sequelize = require("sequelize");
 
 app.use(cors());
 app.use(express.json());
+app.use("/comentarios", rutasComentarios);
+app.use("/usuarios", rutasUsuarios);
+app.use("/publicaciones", rutasPublicaciones);
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'redSocial'
+const sequelize = new Sequelize({
+    dialect: "mysql", //recordatorio: esto indica el tipo de base de datos (mysql, postgree, etc)
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: "redSocial",
+});
+
+sequelize.authenticate()
+.then(() => {
+    console.log("Conexión a la base de datos exitosa.");
+}).catch(error => {
+    console.error("Error al conectar a la base de datos:", error);
 });
 
 app.listen(3991,() => {
-    console.log("corriendo en el puerto 3991");
+    console.log("corriendo en el puerto 3991");/*puerto 3991 para que no tengas conflicto 
+    si usas las vistas en 3000 yuen*/
 });
